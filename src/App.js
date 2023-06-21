2f10db814d520a803ed21345a9ce7e9c67523664
Abhishek Choudhary <theabhishek.srm@gmail.com>
2023-06-21 11:19:20 +0530

    auth

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import Signup from './Components/Signup';
+// import Provider from './context/Provider';
+import A from './Components/A';
+import Login from './Components/Login';
+import Secret from './Components/Secret';
+import {Routes, Route} from "react-router-dom";
 
 const App = () =>{
   
   return(
     <div>
-         <Signup/>
+          <Routes>
+              <Route path="/" element={<Signup/>} />
+              <Route path="/login" element={<Login/>} />
+              <Route path="/secret" element={<Secret/>} />
+          </Routes>
+         
     </div>
   )
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './style.css';
+import React from "react";
+import ReactDOM  from "react-dom";
+import App from "./App";
+import Provider from "./context/Provider";
+import {BrowserRouter} from "react-router-dom";
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  
-    <App />
-  
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-
+ReactDOM.render(
+<Provider>
+     <BrowserRouter>
+        <App/>
+    </BrowserRouter>    
+</Provider>
+, 
+document.getElementById("root"))
